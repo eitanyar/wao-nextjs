a47c7165fb3dfff84b5f347ba5da31612c485233
eitanyar <eitan@wao.co.il>
2026-06-08 23:36:03 +0700

    added GTM

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Rubik, Assistant } from "next/font/google";
+import Script from "next/script";
 import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -105,11 +106,27 @@ export default function RootLayout({
         />
       </head>
       <body style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PQP3PVB"
+            height="0" width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <a href="#main-content" className="skip-link">דלג לתוכן הראשי</a>
         <Header />
         <main id="main-content" style={{ flex: 1 }}>{children}</main>
         <Footer />
         <CookieBanner />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PQP3PVB');`,
+          }}
+        />
       </body>
     </html>
   );
